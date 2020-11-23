241f202a653193c0bc58b4d0494294fde1a75b29
PhanAnh1811 <tuananh.61779@gmail.com>
2020-11-23 22:10:40 +0700

    update

--- Buoi_5/app/src/main/java/com/example/sqlite/MainActivity.java
+++ Buoi_5/app/src/main/java/com/example/sqlite/MainActivity.java
@@ -1,45 +1,138 @@
 package com.example.sqlite;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.text.Editable;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import com.example.sqlite.database.DBManager;
-import com.example.sqlite.model.Student;
+import com.example.sqlite.adapter.MyAdapter;
+import com.example.sqlite.models.Student;
+import com.example.sqlite.adapter.MyAdapter;
+import com.example.sqlite.models.Student;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText mName;
-    private  EditText mClass;
-    private Button mAdd,mUpdate;
-    private DBManager dbManager;
-    private List<Student> studentList;
-    private ListView lvStudent;
+    public static ListView listView;
+    public static ArrayList<Student> studentArrayList;
+    public static MyDatabase database;
+    public static DBManager dbManager;
+    public Button btnSave,btnEdit;
+    private EditText editTextName,editTextClass;
+    private String studentName,studentClass;
+    private static long id = -1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mName=findViewById(R.id.edt_name);
-        mClass=findViewById(R.id.edt_class);
-        mAdd=findViewById(R.id.button_add);
-        mUpdate=findViewById(R.id.button_update);
-        lvStudent=findViewById(R.id.lv_student);
-        mAdd.setOnClickListener(new View.OnClickListener() {
+        dbManager = new DBManager(this);
+        database = new MyDatabase(this);
+        dbManager.hello();
+        initWidget();
+        updateData();
+        btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Student student=new Student();
-                dbManager.addStudent(student);
+            public void onClick(View view) {
+                addStudent();
+            }
+        });
+
+        btnEdit.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                editStudent();
+            }
+        });
+
+    }
+
+    public void initWidget(){
+        editTextName = findViewById(R.id.edt_student_name);
+        editTextClass= findViewById(R.id.edt_student_class);
+        btnSave=findViewById(R.id.btn_add);
+        btnEdit=findViewById(R.id.btn_edit_student);
+        listView = findViewById(R.id.lv_student);
+
+    }
+
+    public void updateData(){
+        if(studentArrayList == null){
+            studentArrayList = new ArrayList<Student>();
+        }else {
+            studentArrayList.removeAll(studentArrayList);
+        }
+
+        Cursor cursor = database.getAllData();
+        if(cursor!=null){
+            while(cursor.moveToNext()){
+                Student student = new Student();
+                student.set_id(Integer.parseInt(cursor.getString(cursor.getColumnIndex(DBManager.ID))));
+                student.setStudentName(cursor.getString(cursor.getColumnIndex(DBManager.NAME)));
+                student.setStudentClass(cursor.getString(cursor.getColumnIndex(DBManager.CLASS)));
+
+                studentArrayList.add(student);
+            }
+        }
+
+        if (studentArrayList !=null){
+            listView.setAdapter(new
+                    MyAdapter(getApplicationContext()));
+        }
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                editTextName.setText(studentArrayList.get(i).getStudentName());
+                editTextClass.setText(studentArrayList.get(i).getStudentClass());
+                MainActivity.id =l;
             }
         });
     }
-    private String generateId(){
-        int rd=(int)Math.random()*999;
-        return "TS"+rd;
+    public Student getStudentInformation(){
+        studentName = editTextName.getText().toString();
+        studentClass = editTextClass.getText().toString();
+        if(studentName.trim().length() ==0 || studentClass.trim().length() == 0) return null;
+        Student student = new Student();
+        student.set_id(id);
+        student.setStudentName(studentName);
+        student.setStudentClass(studentClass);
+        return student;
+    }
+
+    public void addStudent(){
+        Student student1 = getStudentInformation();
+        Log.d("Student",student1.getStudentName() + " * "+ student1.getStudentClass());
+        if(student1!=null){
+            if(database.createStudent(student1) != -1){
+
+                studentArrayList.add(student1);
+                updateData();
+                listView.invalidateViews();
+                editTextName.setText(null);
+                editTextClass.setText(null);
+                id=-1;
+            }
+        }
+    }
+
+    public void editStudent( ){
+        Student student = getStudentInformation();
+        if(student!=null && id!=-1){
+            database.editStudent(student);
+            updateData();
+            listView.invalidateViews();
+            editTextName.setText(null);
+            editTextClass.setText(null);
+            id=-1;
+        }
     }
 }
--- Buoi_6/app/src/main/java/com/example/contentprovider/MainActivity.java
+++ Buoi_6/app/src/main/java/com/example/contentprovider/MainActivity.java
@@ -12,17 +12,35 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnList,btnCall;
     ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCall=findViewById(R.id.btnCuocGoi);
+        btnList=findViewById(R.id.btnDanhBa);
+        listView=findViewById(R.id.listView);
+
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hienThiDanhBa();
+            }
+        });
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nhatKyCuocGoi();
+            }
+        });
     }
-    public void hienThiDanhBa(View view){
+    public void hienThiDanhBa(){
         ArrayList<String> list=new ArrayList<String>();
         Uri uri=null;
         uri= ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
@@ -45,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
         listView.setAdapter(adapter);
     }
-    public void nhatKyCuocGoi(View view){
+    public void nhatKyCuocGoi(){
         ArrayList<String> list=new ArrayList<String>();
         Uri uri = CallLog.Calls.CONTENT_URI;
         String[]projection=new String[]{CallLog.Calls.DATE, CallLog.Calls.NUMBER};
